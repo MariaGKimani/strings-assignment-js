8f16301e1af05126f7d4c8f06d1aff23998f36ad
Mariagoretti <mariakimanigoreti@gmail.com>
2023-03-07 12:35:42 +0300

    strings

---
+++ index.js
@@ -0,0 +1,35 @@
+let girl = "beautiful";
+console.log(girl.length);
+console.log(girl[8]);
+console.log(girl[7]);
+console.log(girl[6]);
+console.log(girl[5]);
+
+const food = "I was feeling hungry today";
+const newstr = "eat"
+console.log(food[4]);
+const result = `I was ${newstr} feeling hungry today`;
+console.log(result)
+
+
+"the"
+"brown"
+const story = "The quick brown fox jumps over the lazy dog";
+console.log(story.match(/the/g).length);
+console.log(story.match(/brown/g).length);
+
+const string5 = "We are now going to school";
+console.log(string5.includes("now"));
+console.log(string5.includes("sitting"));
+
+//convert 
+//1. to Uppercase
+let v = "wonderful"
+console.log(v.toUpperCase())
+//2 . to lowercase
+
+let x = "amazing"
+let y = "BEneath"
+console.log(x.toLocaleLowerCase())
+console.log(y.toLocaleLowerCase())
+//3.TITleCASE
